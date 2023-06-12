88b6610403f5f1fc7f299c5a85fe1a47ee80f9dd
WASIM SOOMRO <soomrowasimw@gmail.com>
2023-06-12 13:53:00 -0600

    FormWork

--- code-301/gallery-of-horns/src/App.js
+++ code-301/gallery-of-horns/src/App.js
@@ -5,6 +5,9 @@ import Footer from './Footer';
 import data from './data/data.json';
 import Modal from 'react-bootstrap/Modal';
 import SelectedBeast from './SelectedBeast';
+import Form from 'react-bootstrap/Form';
+import ListGroup from 'react-bootstrap/ListGroup';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -15,7 +18,11 @@ class App extends React.Component {
     showModal: false,
     selectedBeast: '',
     selectedImage: '',
-    selectedDescription: ''
+    selectedDescription: '',
+    horns: '',
+    myHorns: '',
+    sortedData: data
+
   }
 }
 
@@ -39,16 +46,61 @@ handleCloseModal = () => {
     showModal:false
   })
 }
+
+handleSubmit = (event) => {
+  event.preventDefault();
+  
+  this.setState({
+    horns: event.target.horns.value
+  })
+  console.log('from state in submit handler: ', this.state.horns);
+}
+
+handleInput = (event) => {
+  this.setState({
+    myHorns: event.target.value
+  })
+}
+
+handleSelect = (event) => {
+let selected = parseInt(event.target.value);
+if(selected === "1"){
+  let newData = data.filter(beast => beast.horns === 1);
+  this.setState({
+    sortedData: newData
+  })
+} else if(selected === "2"){
+  let newData = data.filter(beast => beast.horns === 2);
+} else if(selected === "3"){
+  let newData = data.filter(beast => beast.horns === 3);
+} else if(selected === "100") {
+  let newData = data.filter(beast => beast.horns === 100);
+} else if(selected === "all"){
+  this.setState({
+    sortedData: data
+  });
+}
+}
+
 render() {
-  console.log(this.state.selectedBeast);
+  console.log(this.state.sortedData);
   return (
     <>
-    <form>
-      <fieldset>
-        <legend>Number of Horns</legend>
-        
-      </fieldset>
-    </form>
+      <h1> Filter By # of Horns</h1>
+      <Form>
+        <Form.Select onChange={this.handleSelect}>
+          <option value="">Select an Option</option>
+          <option value="">1</option>
+          <option value="">2</option>
+          <option value="">3</option>
+          <option value="">100</option>
+        </Form.Select>
+      </Form>
+      {/* <ListGroup>
+      {this.state.sortedData.map((num, index) => {
+        return <ListGroup.Item key={index}>{num}</ListGroup.Item>
+        })}
+      </ListGroup> */}
     <Header 
     favorites={this.state.favorites}
     />
@@ -70,22 +122,34 @@ render() {
 }
 }
 
-// handleOpenModal = (title, image_url, description) => {
-//   this.setState({
-//     showModal: true,
-//     beastTitle: title,
-//     beastImg: image_url,
-//     beastDescription: description
-//   })
-// }
-// handleCloseModal = () => {
-//   this.setState({
-// showModal: false
-//   })
-// }
-
 
 export default App;
 
 //referenced from in class demo from Gerald Bruno
 //referenced modal code from in class demo from Rocio Martinez
+
+// handleSubmit = (event) => {
+//   event.preventDefault();
+  
+//   this.setState({
+//     horns: event.target.horns.value
+//   })
+//   console.log('from state in submit handler: ', this.state.horns);
+// }
+
+// handleInput = (event) => {
+//   this.setState({
+//     myHorns: event.target.value
+//   })
+// }
+
+// <p> After form submit: {this.state.horns}</p>
+//       <p> On input: {this.state.horns}</p>
+//        <form onSubmit ={this.handleSubmit}>
+//       <fieldset>
+//         <legend>Number of Horns</legend>
+//         <label htmlFor="horns">Number of Horns: </label>
+//         <input id="horns" type="number" name="number" onInput={this.handleInput}></input>
+//       </fieldset>
+//       <button type="submit">Submit</button>
+//     </form>
